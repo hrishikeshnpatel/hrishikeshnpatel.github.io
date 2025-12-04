e689f87806ffee7bda3d5268951a0b3afd1061b0
hrishikeshnpatel <56772809+hrishikeshnpatel@users.noreply.github.com>
2025-12-04 01:06:37 +0000

    Deploying to gh-pages from @ hrishikeshnpatel/hrishikeshnpatel.github.io@8f76e93ea14179b5819c1b376eb2c75c1da641a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+            },{id: "post-a-swiss-dream",
+      
+        title: "A Swiss Dream",
+      
+      description: "A copy of blog post from ERIP&#39;s Intern Blog (https://www.epfl.ch/education/international/en/a-swiss-dream/), about my travels and work in Switzerland in 2022.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2022/A-Swiss-Dream/";
+        
+      },
+    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
       
         title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
